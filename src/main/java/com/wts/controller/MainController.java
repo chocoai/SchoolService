d22a548669bb16021f1b6b52fe66cd2cbd1e3634
王天硕 <nelsonnick@126.com>
2016-12-17 19:42:39 +0800

    a

--- src/main/java/com/wts/controller/MainController.java
+++ src/main/java/com/wts/controller/MainController.java
@@ -1,14 +1,13 @@
 package com.wts.controller;
 
-import com.foxinmy.weixin4j.qy.WeixinProxy;
+import com.foxinmy.weixin4j.exception.WeixinException;
 import com.jfinal.core.Controller;
+import com.wts.entity.*;
 
 public class MainController extends Controller {
 
-  public void index() {
-
-    WeixinProxy wp =new WeixinProxy();
-    renderText(wp.getUserIdByCode(getPara("code")));
+  public void index()  throws WeixinException {
+    renderText(WP.me.getUserByCode(getPara("code")).getName());
 
   }
 }
---
+++ src/main/java/com/wts/entity/WP.java
@@ -0,0 +1,10 @@
+package com.wts.entity;
+
+import com.foxinmy.weixin4j.qy.WeixinProxy;
+
+/**
+ * Created by 王天硕 on 2016/12/17.
+ */
+public class WP {
+    public static final WeixinProxy me = new WeixinProxy();
+}
