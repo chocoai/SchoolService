2be88e405c550a142618c26f168800a64da485ab
王天硕 <nelsonnick@126.com>
2016-12-19 22:33:40 +0800

    a

--- src/main/java/com/wts/controller/MainController.java
+++ src/main/java/com/wts/controller/MainController.java
@@ -1,20 +1,39 @@
 package com.wts.controller;
 
 import com.foxinmy.weixin4j.exception.WeixinException;
+import com.foxinmy.weixin4j.model.media.MediaDownloadResult;
+import com.foxinmy.weixin4j.model.paging.Pageable;
 import com.foxinmy.weixin4j.qy.message.ChatMessage;
 import com.foxinmy.weixin4j.qy.message.CustomeMessage;
 import com.foxinmy.weixin4j.qy.message.NotifyMessage;
+import com.foxinmy.weixin4j.qy.model.IdParameter;
+import com.foxinmy.weixin4j.tuple.Text;
+import com.foxinmy.weixin4j.type.MediaType;
+import com.foxinmy.weixin4j.util.AesToken;
 import com.jfinal.core.Controller;
 import com.wts.entity.*;
+
+import java.io.*;
 
 public class MainController extends Controller {
 
   public void index()  throws WeixinException {
-    //renderText("aaa");
-    NotifyMessage CM = new NotifyMessage();
-    WP.me.;
-    WP.me.sendNotifyMessage();
+//    renderText("aaa");
+    try {
+      //NotifyMessage CM = new NotifyMessage(25,new Text("hello world!!!"),new IdParameter().putUserIds("wts"),false);
+      //WP.me.sendNotifyMessage(CM);
+//上传指定文件
+      //System.out.println(WP.me.uploadMedia(25,new FileInputStream(new File("c:/777.jpg")),"aa"));
+      //System.out.println(WP.me.listMaterialMedia(25, MediaType.image,new Pageable(1,50)).toString());
+      // 创建文件输出流对象
+      FileOutputStream os = new FileOutputStream("c:/a.jpg");
+      os.write(WP.me.downloadMedia(25,"2DYEgQokxhwD197hQGE5L8yMXdkAWaY3YQrpcR5uc1crKnOVs4Kt-Ope4P9xx5jxFvXcYm3MeKJiiUIufeEKqnQ").getContent());
+// 关闭输出流
+      os.close();
 
-    renderText(WP.me.getUserByCode(getPara("code")).getName());
+    } catch (Exception e){
+      System.out.println(e.getMessage());
+    }
+//    renderText(WP.me.getUserByCode(getPara("code")).getName());
   }
 }
