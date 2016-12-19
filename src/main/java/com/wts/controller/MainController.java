30987963b5cee7e8fb6147ce84749e7adc79b086
王天硕 <nelsonnick@126.com>
2016-12-19 16:52:26 +0800

    f

--- src/main/java/com/wts/controller/MainController.java
+++ src/main/java/com/wts/controller/MainController.java
@@ -1,6 +1,9 @@
 package com.wts.controller;
 
 import com.foxinmy.weixin4j.exception.WeixinException;
+import com.foxinmy.weixin4j.qy.message.ChatMessage;
+import com.foxinmy.weixin4j.qy.message.CustomeMessage;
+import com.foxinmy.weixin4j.qy.message.NotifyMessage;
 import com.jfinal.core.Controller;
 import com.wts.entity.*;
 
@@ -8,6 +11,10 @@ public class MainController extends Controller {
 
   public void index()  throws WeixinException {
     //renderText("aaa");
+    NotifyMessage CM = new NotifyMessage();
+    WP.me.;
+    WP.me.sendNotifyMessage();
+
     renderText(WP.me.getUserByCode(getPara("code")).getName());
   }
 }
