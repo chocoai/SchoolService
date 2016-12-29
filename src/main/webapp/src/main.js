1bb1bb39acf113c93ee5621f2df91c371625ee3d
王天硕 <nelsonnick@126.com>
2016-12-29 11:54:05 +0800

    t

--- src/main/webapp/src/main.js
+++ src/main/webapp/src/main.js
@@ -2,7 +2,8 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
 import App from './App'
-
+import Mint from 'mint-ui';
+Vue.use(Mint);
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
